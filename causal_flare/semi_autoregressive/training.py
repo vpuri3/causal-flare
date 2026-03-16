27d99c2991838f228bbe40736e37844336841177
Vedant Puri <vedantpuri@gmail.com>
2026-03-15 22:14:00 -0400

    Fuse semi-AR scan and block-z forward path

--- causal_flare/semi_autoregressive/training.py
+++ causal_flare/semi_autoregressive/training.py
@@ -6,8 +6,6 @@ from causal_flare._reference_utils import (
 from causal_flare.autoregressive.training import _profiled_call, _refresh_profile_totals, _resolve_forward_launch
 from causal_flare.semi_autoregressive.reference import _validate_block_causal_config
 
-_LN2 = 0.6931471824645996
-
 try:
     from triton.tools.tensor_descriptor import TensorDescriptor
 except Exception:
@@ -254,15 +252,10 @@ def _get_semi_ar_forward_config(
         default_num_warps=int(bucket_defaults["prepare_launch"][0]),
         default_num_stages=int(bucket_defaults["prepare_launch"][1]),
     )
-    scan_warps, scan_stages = _resolve_forward_launch(
-        "semi_ar_scan",
+    scan_z_warps, scan_z_stages = _resolve_forward_launch(
+        "semi_ar_scan_z",
         default_num_warps=8,
         default_num_stages=2,
-    )
-    block_z_warps, block_z_stages = _resolve_forward_launch(
-        "semi_ar_block_z",
-        default_num_warps=4,
-        default_num_stages=1,
     )
     lse_output_warps, lse_output_stages = _resolve_forward_launch(
         "semi_ar_lse_output",
@@ -287,10 +280,8 @@ def _get_semi_ar_forward_config(
         "input_precision": _normalize_input_precision(input_precision, None),
         "block_prepare_num_warps": block_prepare_warps,
         "block_prepare_num_stages": block_prepare_stages,
-        "scan_num_warps": scan_warps,
-        "scan_num_stages": scan_stages,
-        "block_z_num_warps": block_z_warps,
-        "block_z_num_stages": block_z_stages,
+        "scan_z_num_warps": scan_z_warps,
+        "scan_z_num_stages": scan_z_stages,
         "lse_output_num_warps": lse_output_warps,
         "lse_output_num_stages": lse_output_stages,
     }
@@ -835,14 +826,14 @@ def semi_ar_block_prepare_desc_kernel(
     )
 
 @triton.jit
-def semi_ar_block_scan_kernel(
+def semi_ar_block_scan_z_kernel(
     BlockMax_ptr,
     BlockDen_ptr,
     BlockNum_ptr,
     PrefixMax_ptr,
     PrefixDen_ptr,
-    PrefixNum_ptr,
     LSEEnc_ptr,
+    ZBlock_ptr,
     stride_bmax_bh,
     stride_bmax_blk,
     stride_bmax_m,
@@ -859,13 +850,13 @@ def semi_ar_block_scan_kernel(
     stride_pden_bh,
     stride_pden_blk,
     stride_pden_m,
-    stride_pnum_bh,
-    stride_pnum_blk,
-    stride_pnum_m,
-    stride_pnum_d,
     stride_lsee_bh,
     stride_lsee_blk,
     stride_lsee_m,
+    stride_z_bh,
+    stride_z_blk,
+    stride_z_m,
+    stride_z_d,
     BH,
     M,
     D_VALUE: tl.constexpr,
@@ -898,14 +889,8 @@ def semi_ar_block_scan_kernel(
     while block_idx < NUM_BLOCKS:
         pmax_ptr = PrefixMax_ptr + pid_bh * stride_pmax_bh + block_idx * stride_pmax_blk
         pden_ptr = PrefixDen_ptr + pid_bh * stride_pden_bh + block_idx * stride_pden_blk
-        pnum_ptr = PrefixNum_ptr + pid_bh * stride_pnum_bh + block_idx * stride_pnum_blk
         tl.store(pmax_ptr + m_offsets * stride_pmax_m, prefix_max, mask=store_shared & mask_m)
         tl.store(pden_ptr + m_offsets * stride_pden_m, prefix_den, mask=store_shared & mask_m)
-        tl.store(
-            pnum_ptr + m_offsets[:, None] * stride_pnum_m + d_offsets[None, :] * stride_pnum_d,
-            prefix_num.to(num_dtype),
-            mask=mask_md,
-        )
 
         bmax_ptr = BlockMax_ptr + pid_bh * stride_bmax_bh + block_idx * stride_bmax_blk
         bden_ptr = BlockDen_ptr + pid_bh * stride_bden_bh + block_idx * stride_bden_blk
@@ -927,112 +912,28 @@ def semi_ar_block_scan_kernel(
         scale_prev = tl.where(both_inf & (prefix_max == -float("inf")), 1.0, scale_prev)
         scale_block = tl.where(both_inf & (block_max == -float("inf")), 1.0, scale_block)
 
-        prefix_den = prefix_den * scale_prev + block_den * scale_block
-        prefix_num = prefix_num * scale_prev[:, None] + block_num * scale_block[:, None]
-        prefix_max = max_new
+        full_den = prefix_den * scale_prev + block_den * scale_block
+        full_num = prefix_num * scale_prev[:, None] + block_num * scale_block[:, None]
+        full_lse_log2 = max_new + tl.math.log2(tl.maximum(full_den, 1e-20))
+        full_lse_log2_safe = tl.where(full_lse_log2 == -float("inf"), 0.0, full_lse_log2)
+        prefix_scale = tl.where(prefix_max == -float("inf"), 0.0, tl.math.exp2(prefix_max - full_lse_log2_safe))
+        block_scale = tl.where(block_max == -float("inf"), 0.0, tl.math.exp2(block_max - full_lse_log2_safe))
+        z_md = prefix_num * prefix_scale[:, None] + block_num * block_scale[:, None]
 
         lse_ptr = LSEEnc_ptr + pid_bh * stride_lsee_bh + block_idx * stride_lsee_blk
-        lse = (prefix_max + tl.math.log2(tl.maximum(prefix_den, 1e-20))) * LN2
-        tl.store(lse_ptr + m_offsets * stride_lsee_m, lse, mask=store_shared & mask_m)
+        tl.store(lse_ptr + m_offsets * stride_lsee_m, full_lse_log2 * LN2, mask=store_shared & mask_m)
+
+        z_ptr = ZBlock_ptr + pid_bh * stride_z_bh + block_idx * stride_z_blk
+        tl.store(
+            z_ptr + m_offsets[:, None] * stride_z_m + d_offsets[None, :] * stride_z_d,
+            z_md.to(num_dtype),
+            mask=mask_md,
+        )
+
+        prefix_max = max_new
+        prefix_den = full_den
+        prefix_num = full_num
         block_idx += 1
-
-
-@triton.jit
-def semi_ar_block_z_kernel(
-    BlockMax_ptr,
-    BlockNum_ptr,
-    PrefixMax_ptr,
-    PrefixNum_ptr,
-    LSEEnc_ptr,
-    ZBlock_ptr,
-    stride_bmax_bh,
-    stride_bmax_blk,
-    stride_bmax_m,
-    stride_bnum_bh,
-    stride_bnum_blk,
-    stride_bnum_m,
-    stride_bnum_d,
-    stride_pmax_bh,
-    stride_pmax_blk,
-    stride_pmax_m,
-    stride_pnum_bh,
-    stride_pnum_blk,
-    stride_pnum_m,
-    stride_pnum_d,
-    stride_lsee_bh,
-    stride_lsee_blk,
-    stride_lsee_m,
-    stride_z_bh,
-    stride_z_blk,
-    stride_z_m,
-    stride_z_d,
-    BH,
-    M,
-    D_VALUE: tl.constexpr,
-    NUM_BLOCKS,
-    BLOCK_M: tl.constexpr,
-    BLOCK_D: tl.constexpr,
-):
-    RCP_LN2: tl.constexpr = 1.4426950408889634
-    pid_bh = tl.program_id(0)
-    block_idx = tl.program_id(1)
-    pid_md = tl.program_id(2)
-    if pid_bh >= BH:
-        return
-
-    num_d_tiles = tl.cdiv(D_VALUE, BLOCK_D)
-    pid_m = pid_md // num_d_tiles
-    pid_d = pid_md - pid_m * num_d_tiles
-
-    m_offsets = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
-    d_offsets = pid_d * BLOCK_D + tl.arange(0, BLOCK_D)
-    mask_m = m_offsets < M
-    mask_d = d_offsets < D_VALUE
-    mask_md = mask_m[:, None] & mask_d[None, :]
-
-    block_max = tl.load(
-        BlockMax_ptr + pid_bh * stride_bmax_bh + block_idx * stride_bmax_blk + m_offsets * stride_bmax_m,
-        mask=mask_m,
-        other=-float("inf"),
-    ).to(tl.float32)
-    block_num = tl.load(
-        BlockNum_ptr
-        + pid_bh * stride_bnum_bh
-        + block_idx * stride_bnum_blk
-        + m_offsets[:, None] * stride_bnum_m
-        + d_offsets[None, :] * stride_bnum_d,
-        mask=mask_md,
-        other=0.0,
-    ).to(tl.float32)
-    prefix_max = tl.load(
-        PrefixMax_ptr + pid_bh * stride_pmax_bh + block_idx * stride_pmax_blk + m_offsets * stride_pmax_m,
-        mask=mask_m,
-        other=-float("inf"),
-    ).to(tl.float32)
-    prefix_num = tl.load(
-        PrefixNum_ptr
-        + pid_bh * stride_pnum_bh
-        + block_idx * stride_pnum_blk
-        + m_offsets[:, None] * stride_pnum_m
-        + d_offsets[None, :] * stride_pnum_d,
-        mask=mask_md,
-        other=0.0,
-    ).to(tl.float32)
-    lse_enc = tl.load(
-        LSEEnc_ptr + pid_bh * stride_lsee_bh + block_idx * stride_lsee_blk + m_offsets * stride_lsee_m,
-        mask=mask_m,
-        other=-float("inf"),
-    ).to(tl.float32)
-
-    lse_enc_log2 = lse_enc * RCP_LN2
-    prefix_scale = tl.math.exp2(prefix_max - lse_enc_log2)
-    prefix_scale = tl.where(prefix_max == -float("inf"), 0.0, prefix_scale)
-    block_scale = tl.math.exp2(block_max - lse_enc_log2)
-    block_scale = tl.where(block_max == -float("inf"), 0.0, block_scale)
-    z_md = prefix_num * prefix_scale[:, None] + block_num * block_scale[:, None]
-
-    z_ptr = ZBlock_ptr + pid_bh * stride_z_bh + block_idx * stride_z_blk
-    tl.store(z_ptr + m_offsets[:, None] * stride_z_m + d_offsets[None, :] * stride_z_d, z_md, mask=mask_md)
 
 
 @triton.jit
@@ -1618,7 +1519,7 @@ def _run_semi_ar_block_prepare_phase(
     return block_max, block_den, block_num
 
 
-def _run_semi_ar_block_scan_phase(
+def _run_semi_ar_block_scan_z_phase(
     block_max: torch.Tensor,
     block_den: torch.Tensor,
     block_num: torch.Tensor,
@@ -1636,84 +1537,35 @@ def _run_semi_ar_block_scan_phase(
         return (
             torch.empty((BH, config["NUM_BLOCKS"], M), device=device, dtype=torch.float32),
             torch.empty((BH, config["NUM_BLOCKS"], M), device=device, dtype=torch.float32),
-            torch.empty((BH, config["NUM_BLOCKS"], M, D_value), device=device, dtype=num_storage_dtype),
             torch.empty((BH, config["NUM_BLOCKS"], M), device=device, dtype=torch.float32),
         )
 
-    prefix_max, prefix_den, prefix_num, lse_enc = _profiled_call(
+    prefix_max, prefix_den, lse_enc = _profiled_call(
         device, kernel_timings, "alloc_scan_stats", alloc_prefix_stats
     )
+
+    def alloc_block_z():
+        return torch.empty((BH, config["NUM_BLOCKS"], M, D_value), device=device, dtype=num_storage_dtype)
+
+    z_block = _profiled_call(device, kernel_timings, "alloc_block_z", alloc_block_z)
 
     def grid(_meta):
         return (BH, config["NUM_M_TILES"], config["NUM_D_VALUE_BLOCKS"])
 
     def launch():
-        semi_ar_block_scan_kernel[grid](
+        semi_ar_block_scan_z_kernel[grid](
             block_max,
             block_den,
             block_num,
             prefix_max,
             prefix_den,
-            prefix_num,
             lse_enc,
+            z_block,
             *block_max.stride(),
             *block_den.stride(),
             *block_num.stride(),
             *prefix_max.stride(),
             *prefix_den.stride(),
-            *prefix_num.stride(),
-            *lse_enc.stride(),
-            BH,
-            M,
-            D_value,
-            config["NUM_BLOCKS"],
-            BLOCK_M=config["BLOCK_M"],
-            BLOCK_D=config["BLOCK_D"],
-            **_num_storage_kernel_flags(num_storage_dtype),
-            num_warps=config["scan_num_warps"],
-            num_stages=config["scan_num_stages"],
-        )
-
-    _profiled_call(device, kernel_timings, "semi_ar_block_scan", launch)
-    return prefix_max, prefix_den, prefix_num, lse_enc
-
-
-def _run_semi_ar_block_z_phase(
-    block_max: torch.Tensor,
-    block_num: torch.Tensor,
-    prefix_max: torch.Tensor,
-    prefix_num: torch.Tensor,
-    lse_enc: torch.Tensor,
-    *,
-    M: int,
-    D_value: int,
-    config: dict[str, object],
-    kernel_timings: dict[str, float] | None = None,
-):
-    BH = block_max.size(0)
-    device = block_max.device
-    z_dtype = block_num.dtype
-
-    def alloc_block_z():
-        return torch.empty((BH, config["NUM_BLOCKS"], M, D_value), device=device, dtype=z_dtype)
-
-    z_block = _profiled_call(device, kernel_timings, "alloc_block_z", alloc_block_z)
-
-    def grid(_meta):
-        return (BH, config["NUM_BLOCKS"], config["NUM_M_TILES"] * config["NUM_D_VALUE_BLOCKS"])
-
-    def launch():
-        semi_ar_block_z_kernel[grid](
-            block_max,
-            block_num,
-            prefix_max,
-            prefix_num,
-            lse_enc,
-            z_block,
-            *block_max.stride(),
-            *block_num.stride(),
-            *prefix_max.stride(),
-            *prefix_num.stride(),
             *lse_enc.stride(),
             *z_block.stride(),
             BH,
@@ -1722,12 +1574,13 @@ def _run_semi_ar_block_z_phase(
             config["NUM_BLOCKS"],
             BLOCK_M=config["BLOCK_M"],
             BLOCK_D=config["BLOCK_D"],
-            num_warps=config["block_z_num_warps"],
-            num_stages=config["block_z_num_stages"],
+            **_num_storage_kernel_flags(num_storage_dtype),
+            num_warps=config["scan_z_num_warps"],
+            num_stages=config["scan_z_num_stages"],
         )
 
-    _profiled_call(device, kernel_timings, "semi_ar_block_z", launch)
-    return z_block
+    _profiled_call(device, kernel_timings, "semi_ar_block_scan_z", launch)
+    return prefix_max, prefix_den, lse_enc, z_block
 
 
 def _semi_autoregressive_forward_triton(
@@ -1742,6 +1595,7 @@ def _semi_autoregressive_forward_triton(
     profile: bool = False,
     Q_dec: torch.Tensor | None = None,
     K_dec: torch.Tensor | None = None,
+    return_backward_state: bool = False,
 ):
     B, N, H, M, D_score, D_value = _validate_flare_qkv_layouts(Q, K, V, name="SemiAutoRegressiveFLARE")
     block_size, chunk_size, _, _, _ = _validate_block_causal_config(
@@ -1787,21 +1641,10 @@ def _semi_autoregressive_forward_triton(
         kernel_timings=timing_bucket,
     )
 
-    prefix_max, prefix_den, prefix_num, lse_enc = _run_semi_ar_block_scan_phase(
+    prefix_max, prefix_den, lse_enc, z_block = _run_semi_ar_block_scan_z_phase(
         block_max,
         block_den,
         block_num,
-        M=M,
-        D_value=D_value,
-        config=cfg,
-        kernel_timings=timing_bucket,
-    )
-    z_block = _run_semi_ar_block_z_phase(
-        block_max,
-        block_num,
-        prefix_max,
-        prefix_num,
-        lse_enc,
         M=M,
         D_value=D_value,
         config=cfg,
@@ -1831,13 +1674,23 @@ def _semi_autoregressive_forward_triton(
     aux = {
         "LSE_dec": lse_dec.view(B, H, N),
         "LSE_enc": lse_enc.view(B, H, cfg["NUM_BLOCKS"], M),
-        "prefix_max": (prefix_max * _LN2).view(B, H, cfg["NUM_BLOCKS"], M),
-        "prefix_den": prefix_den.view(B, H, cfg["NUM_BLOCKS"], M),
     }
+    backward_state = None
+    if return_backward_state:
+        backward_state = {
+            "prefix_max": prefix_max,
+            "prefix_den": prefix_den,
+            "lse_enc": lse_enc,
+            "lse_dec": lse_dec,
+        }
 
     if profile_data is not None:
         _refresh_profile_totals(profile_data)
+        if return_backward_state:
+            return Y, aux, profile_data, backward_state
         return Y, aux, profile_data
+    if return_backward_state:
+        return Y, aux, backward_state
     return Y, aux
 
 
@@ -1856,8 +1709,28 @@ class SemiAutoRegressiveFLARE(autograd.Function):
         Q_dec=None,
         K_dec=None,
     ):
-        del ctx
-        return _semi_autoregressive_forward_triton(
+        if profile:
+            Y, aux, profile_data, backward_state = _semi_autoregressive_forward_triton(
+                Q,
+                K,
+                V,
+                block_size=block_size,
+                chunk_size=chunk_size,
+                scale=scale,
+                input_precision=input_precision,
+                profile=profile,
+                Q_dec=Q_dec,
+                K_dec=K_dec,
+                return_backward_state=True,
+            )
+            ctx.save_for_backward(
+                backward_state["prefix_max"],
+                backward_state["prefix_den"],
+                backward_state["lse_enc"],
+                backward_state["lse_dec"],
+            )
+            return Y, aux, profile_data
+        Y, aux, backward_state = _semi_autoregressive_forward_triton(
             Q,
             K,
             V,
@@ -1868,7 +1741,15 @@ class SemiAutoRegressiveFLARE(autograd.Function):
             profile=profile,
             Q_dec=Q_dec,
             K_dec=K_dec,
+            return_backward_state=True,
         )
+        ctx.save_for_backward(
+            backward_state["prefix_max"],
+            backward_state["prefix_den"],
+            backward_state["lse_enc"],
+            backward_state["lse_dec"],
+        )
+        return Y, aux
 
     @staticmethod
     def backward(ctx, dY, *unused):
